137d9e2bbea3eff43666683c53fe6c3159096274
DonAnk <11donank22@gmail.com>
2021-06-05 22:17:09 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import questions from './questions.json';
 
 const firstQuestionArray = questions.beginner[0].question.split('blank')
 
-
 function App() {
   console.log(firstQuestionArray)
   return (
     <div className="App">
       <header className="App-header">
         <div>{
-          firstQuestionArray.map(element => {
+          firstQuestionArray.filter(it => it != "").map(element => {
             return (
               <div>
                 <p>{element}</p>
